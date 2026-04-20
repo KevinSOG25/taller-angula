c8f1cd9f692839df44eb134339e196294f97012d
Kevin Oviedo <k.oviedog@uniandes.edu.co>
2026-04-20 07:36:02 -0500

    cambios base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { SerieList } from './series/serie-list/serie-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: SerieList },
+];
--- src/app/series/serie-list/serie-list.ts
+++ src/app/series/serie-list/serie-list.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SerieService } from '../serie.service';
+import { Serie } from '../serie';
 
 @Component({
   selector: 'app-serie-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './serie-list.html',
   styleUrl: './serie-list.css',
 })
-export class SerieList {
+export class SerieList implements OnInit {
 
+  series: Serie[] = [];
+
+  constructor(private serieService: SerieService) { }
+
+  ngOnInit(): void {
+    this.serieService.getSeries().subscribe(data => {
+      this.series = data;
+    });
+  }
 }
