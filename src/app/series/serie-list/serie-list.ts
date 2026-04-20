98b5957d81fdcdfcbae1261ee42c4c8b7dddaccc
Kevin Oviedo <k.oviedog@uniandes.edu.co>
2026-04-20 06:54:55 -0500

    Arreglo

---
+++ src/app/series/serie-list/serie-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-serie-list',
+  imports: [],
+  templateUrl: './serie-list.html',
+  styleUrl: './serie-list.css',
+})
+export class SerieList {
+
+}
