6e592acf618db70ea6a7f3e88f27ac05cf55e240
Kevin Oviedo <k.oviedog@uniandes.edu.co>
2026-04-20 20:05:34 -0500

    Reto independiente y respuesta a las preguntas

--- src/app/series/serie-list/serie-list.ts
+++ src/app/series/serie-list/serie-list.ts
@@ -12,12 +12,25 @@ import { Serie } from '../serie';
 export class SerieList implements OnInit {
 
   series: Serie[] = [];
+  selectedSerie: Serie | null = null;
+  averageSeasons: number = 0;
 
   constructor(private serieService: SerieService) { }
 
   ngOnInit(): void {
     this.serieService.getSeries().subscribe(data => {
       this.series = data;
+      this.calculateAverage();
     });
+  }
+
+  calculateAverage(): void {
+    if (this.series.length > 0) {
+      this.averageSeasons = this.series.reduce((sum, s) => sum + s.seasons, 0) / this.series.length;
+    }
+  }
+
+  selectSerie(serie: Serie): void {
+    this.selectedSerie = serie;
   }
 }
